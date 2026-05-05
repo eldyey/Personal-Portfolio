bca099c812a94a437403dc84cbdca725a120796b
INTERN10-LMANZANERO <intern10-lmanzanero@INTERN10-LMANZANERO.local>
2026-05-05 11:13:54 +0800

    Fix web deploy

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2506453779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "169374040" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
